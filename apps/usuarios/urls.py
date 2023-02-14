3c2b566129e8b9259f833286f7b49f797fb1050b
SinvalFilho <sinvalleitefilho@gmail.com>
2023-02-14 03:46:47 -0300

    first commit

---
+++ apps/usuarios/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views  as auth_views
+
+urlpatterns = [
+      path('cadastro/', views.cadastro, name="cadastro" ),
+      path('login/', views.login, name="login"),
+      path('dashboard', views.dashboard, name="dashboard"),
+      path('logout/', views.logout, name="logout"),
+
+    
+
+
+
+
+      
+
+      
+]  
